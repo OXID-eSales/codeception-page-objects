937acb65c1385d3a140dc7ecf5216bdb6eb49d23
lambreva <aneliya.lambreva@dnhsoft.com>
2022-11-17 10:05:16 +0200

    OXDEV-5750 OXDEV-6317 Add footer widgets components

---
+++ Page/Component/Footer/ManufacturersWidget.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\Codeception\Page\Component\Footer;
+
+/**
+ * Trait for manufacturers menu widget in footer
+ * @package OxidEsales\Codeception\Page\Component\Footer
+ */
+trait ManufacturersWidget
+{
+    public $manufacturersWrapper = '.footer-box-manufacturers';
+}
