8c7359c91f2d8b0e6d6dda2cd0d07c12bc04ae1a
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2023-05-31 12:09:46 +0200

    Revert "OXDEV-3190 Update cookie note CEST"
    This reverts commit 5e3c9c67ca964f039a976aea5771d8b430e2739a.

--- Page/Home.php
+++ Page/Home.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace OxidEsales\Codeception\Page;
 
-use OxidEsales\Codeception\Page\Component\CookieNotice;
 use OxidEsales\Codeception\Page\Component\Footer\Footer;
 use OxidEsales\Codeception\Page\Component\Header\Header;
 use OxidEsales\Codeception\Page\Lists\ProductList;
@@ -32,9 +31,4 @@ class Home extends Page
         return $productListPage;
     }
 
-    public function getCookieNotice(): ?CookieNotice
-    {
-        $I = $this->user;
-        return new CookieNotice($I);
-    }
 }
