13395d067277678c7f5cae2d6cc8e968838e17ab
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2023-09-29 17:40:24 +0200

    OXDEV-7363 Switch translation domains in Setup Finish step

--- ShopSetup/FinishStep.php
+++ ShopSetup/FinishStep.php
@@ -11,6 +11,7 @@ namespace OxidEsales\Codeception\ShopSetup;
 
 use Codeception\Actor;
 use OxidEsales\Codeception\Admin\AdminLoginPage;
+use OxidEsales\Codeception\Module\Translation\Translator;
 use OxidEsales\Codeception\Page\Home;
 
 class FinishStep extends SetupStep
@@ -25,6 +26,9 @@ class FinishStep extends SetupStep
 
     public function openShop(Actor $IShop): Home
     {
+        Translator::switchTranslationDomain(
+            Translator::TRANSLATION_DOMAIN_SHOP
+        );
         $homePage = new Home($IShop);
         $IShop->click($this->shopLinkButton);
         $IShop->waitForPageLoad();
@@ -34,6 +38,9 @@ class FinishStep extends SetupStep
 
     public function openAdmin(Actor $IAdmin): AdminLoginPage
     {
+        Translator::switchTranslationDomain(
+            Translator::TRANSLATION_DOMAIN_ADMIN
+        );
         $adminLogin = new AdminLoginPage($IAdmin);
         $IAdmin->click($this->adminLinkButton);
         $IAdmin->waitForPageLoad();
