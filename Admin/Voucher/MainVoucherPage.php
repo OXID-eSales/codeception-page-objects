eb8357a714f3b2697fcbccce82963800c8cc6433
RahatHameed <the_rahat_online@yahoo.com>
2024-10-23 10:59:53 +0200

    OXDEV-8842 Checked discount and allow same series fields are disabled

--- Admin/Voucher/MainVoucherPage.php
+++ Admin/Voucher/MainVoucherPage.php
@@ -19,6 +19,9 @@ class MainVoucherPage extends Page
     public string $titleInput = "//input[@name='editval[oxvoucherseries__oxserienr]']";
     public string $voucherType = "//select[@name='editval[oxvoucherseries__oxdiscounttype]']";
     public string $saveButton = "//input[@name='save']";
+	public string $discountField = "//input[@name='editval[oxvoucherseries__oxdiscount]']";
+	public string $allowSameSeriesYes = "//input[@name='editval[oxvoucherseries__oxallowsameseries]'][@value='1']";
+	public string $allowSameSeriesNo = "//input[@name='editval[oxvoucherseries__oxallowsameseries]'][@value='0']";
 
     public function createVoucher(Voucher $voucher)
     {
@@ -41,4 +44,30 @@ class MainVoucherPage extends Page
 
         return $this;
     }
+
+	public function checkVoucherDiscountFieldForShipfreeVoucher(): void
+	{
+		$I = $this->user;
+
+		$I->selectOption($this->voucherType, 'shipfree');
+
+		$I->waitForElementVisible($this->discountField, 5);
+
+		$I->seeElement($this->discountField);
+		$I->seeElement($this->discountField, ['disabled' => 'true']);
+		$I->seeInField($this->discountField, '0');
+	}
+
+	public function checkAllowSameSeriesRadioDisabled(): self
+	{
+		$I = $this->user;
+
+		$I->waitForElementVisible($this->allowSameSeriesYes, 5);
+
+		$I->seeCheckboxIsChecked($this->allowSameSeriesNo);
+		$I->seeElement($this->allowSameSeriesYes, ['disabled' => 'true']);
+		$I->dontSeeElement($this->allowSameSeriesNo, ['disabled' => 'true']);
+
+		return $this;
+	}
 }
