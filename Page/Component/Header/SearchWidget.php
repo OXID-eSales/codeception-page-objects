ef8d980b933df06499621f93b92dab6631d6d39c
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2025-09-16 10:01:12 +0200

    Backport searchFor fix

--- Page/Component/Header/SearchWidget.php
+++ Page/Component/Header/SearchWidget.php
@@ -11,30 +11,22 @@ namespace OxidEsales\Codeception\Page\Component\Header;
 
 use OxidEsales\Codeception\Page\Lists\ProductSearchList;
 
-/**
- * Trait for the search widget in the header.
- * @package OxidEsales\Codeception\Page\Component\Header
- */
+use function sprintf;
+
 trait SearchWidget
 {
     public $searchField = '#searchParam';
+    public $searchButton = 'button[type=submit]';
+    public $searchForm = 'form[name=search]';
 
-    public $searchButton = '';
-
-    public $searchForm = '//form[name=search]';
-
-    /**
-     * Executes the search and opens result page.
-     *
-     * @param string $value
-     *
-     * @return ProductSearchList
-     */
+    /** @return ProductSearchList */
     public function searchFor(string $value)
     {
         $I = $this->user;
         $I->fillField($this->searchField, $value);
-        $I->click('form[name=search] button[type=submit]');
+        $button = sprintf('%s %s', $this->searchForm, $this->searchButton);
+        $I->click($button);
+        $I->waitForElementClickable($button);
         return new ProductSearchList($I);
     }
 }
